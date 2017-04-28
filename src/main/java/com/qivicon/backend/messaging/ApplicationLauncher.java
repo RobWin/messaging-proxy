f90583e5d6eec5a3ae8a71f2a041e7723baf17d5
Robert Winkler <rwinkler@telekom.de>
2017-04-28 10:42:51 +0200

    Configured vertx-maven-plugin

--- src/main/java/com/qivicon/backend/messaging/ApplicationLauncher.java
+++ src/main/java/com/qivicon/backend/messaging/ApplicationLauncher.java
@@ -3,12 +3,16 @@ package com.qivicon.backend.messaging;
 import com.qivicon.backend.messaging.verticles.ApplicationVerticle;
 import io.vertx.core.VertxOptions;
 import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.qivicon.backend.messaging.logging.LoggingUtils.configureLogging;
 
 public class ApplicationLauncher extends io.vertx.core.Launcher {
 
     public static final String REGISTRY_NAME = "metricsRegistry";
+
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationLauncher.class);
 
     /**
      * Main entry point.
@@ -17,6 +21,11 @@ public class ApplicationLauncher extends io.vertx.core.Launcher {
      */
     public static void main(String[] args) {
         configureLogging();
+        LOG.info("Run application launcher");
+        for(String arg : args){
+            LOG.info("Argument: {}", arg);
+        }
+
         new ApplicationLauncher().dispatch(args);
     }
 
